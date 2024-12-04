ec8afd2e8a3ebbfecc53676f8ba2aeb5606fea24
John Hawthorn <john@hawthorn.email>
2024-12-03 22:21:36 -0800

    Initial commit

---
+++ lib/regex_audit.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "regex_audit/version"
+
+module RegexAudit
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_regex_audit.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestRegexAudit < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::RegexAudit::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
